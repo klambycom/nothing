8b0b4cbcb1d96971565c914bf558aaa86616f9df
klambycom <christian@klamby.com>
2015-05-26 23:54:24 +0200

    Code

---
+++ source/ResearchServicePlatform/src/service/utility/Time.java
@@ -0,0 +1,12 @@
+package service.utility;
+
+/**
+ * Responsible for managing relationship between actual time and logical time 
+ * @author Yifan Ruan (ry222ad@student.lnu.se)
+ */
+public class Time {
+	/**
+	 * Scaling time with this parameter
+	 */
+	public static int scale=100;   
+}
---
+++ source/TeleAssistanceSystem/src/tas/services/profiles/ServiceDelayProfile.java
@@ -0,0 +1,36 @@
+package tas.services.profiles;
+
+import java.util.Random;
+
+import service.atomic.ServiceProfile;
+import service.atomic.ServiceProfileAttribute;
+import service.utility.Time;
+	
+public class ServiceDelayProfile extends ServiceProfile{
+
+	@ServiceProfileAttribute
+	public int minDelay;
+	
+	@ServiceProfileAttribute
+	public int maxDelay;
+    
+	Random random = new Random();
+	
+	@Override
+	public boolean preInvokeOperation(String operationName, Object... args) {
+	    
+	    try {
+		Thread.sleep((random.nextInt(maxDelay - minDelay + 1) + minDelay)*Time.scale);
+	    } catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    }
+		return true;
+	}
+
+	@Override
+	public Object postInvokeOperation(String operationName, Object result,
+			Object... args) {
+		return result;
+	}
+}
