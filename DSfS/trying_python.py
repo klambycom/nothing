5c36a449afd74c0dc1223fabcc303e37b98d20a0
klambycom <christian@klamby.com>
2015-06-08 21:40:09 +0200

    Functions

--- DSfS/trying_python.py
+++ DSfS/trying_python.py
@@ -12,3 +12,21 @@ import re as regex
 my_regex = regex.compile("[0-9]+", regex.I)
 
 print 5 / 2
+
+def double(x):
+    """this where you put an optional docstring that explains what the
+    function does. For example, this function multiplies its input by 2"""
+    return x * 2
+
+def my_print(message='my default message'):
+    print message
+
+my_print()
+my_print(double(10))
+
+def subtract(a=0, b=0):
+    return a - b
+
+print subtract(10, 5)
+print subtract(0, 5)
+print subtract(b=5)
