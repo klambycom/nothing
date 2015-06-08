3cf8bf460d760fa459cfeb3f9741f5d200d64574
klambycom <christian@klamby.com>
2015-06-08 20:01:20 +0200

    Imports

--- DSfS/trying_python.py
+++ DSfS/trying_python.py
@@ -1,7 +1,14 @@
-for i in [1, 2, 3, 4, 5]:
-    print i
-    for j in [1, 2, 3, 4, 5]:
-        print j
-        print i + j
-    print i
-print "done looping"
+#for i in [1, 2, 3, 4, 5]:
+#    print i
+#    for j in [1, 2, 3, 4, 5]:
+#        print j
+#        print i + j
+#    print i
+#print "done looping"
+
+from __future__ import division
+import re as regex
+
+my_regex = regex.compile("[0-9]+", regex.I)
+
+print 5 / 2
