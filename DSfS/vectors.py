962fefad39671de2c73dc89abfc8d9b73c5481f8
klambycom <christian@klamby.com>
2015-06-09 00:30:15 +0200

    Vectors

---
+++ DSfS/vectors.py
@@ -0,0 +1,62 @@
+# -*- coding: iso-8859-15 -*-
+
+from __future__ import division
+import re, math, random
+from collections import defaultdict, Counter
+
+#
+# functions for working with vectors
+#
+
+def vector_add(v, w):
+    """adds corresponding elements"""
+    return [v_i + w_i for v_i, w_i in zip(v, w)]
+
+def vector_subtract(v, w):
+    """adds corresponding elements"""
+    return [v_i - w_i for v_i, w_i in zip(v, w)]
+
+def vector_sum(vectors):
+    """sums all corresponding elements"""
+    return reduce(vector_add, vectors)
+
+def scalar_multiply(n, vector):
+    """n is a number, and vector is vector"""
+    return [n * v for v in vector]
+
+def vector_mean(vectors):
+    """compute the vector whose ith element is the mean fo the ith elements
+    of the input vectors"""
+    n = len(vectors)
+    return scalar_multiply(1/n, vector_sum(vectors))
+
+def dot(v, w):
+    """v_1 * w_1 + ... + v_n * w_n"""
+    return sum(v_i * w_i for v_i, w_i in zip(v, w))
+
+def sum_of_squares(v):
+    """v_1 * v_1 + ... + v_n * v_n"""
+    return dot(v, v)
+
+def magnitude(v):
+    return math.sqrt(sum_of_squares(v))
+
+def squared_distance(v, w):
+    """(v_1 - w_1) ** 2 + .. + (v_n - w_n) ** 2"""
+    return sum_of_squares(vector_subtract(v, w))
+
+def distance(v, w):
+    return magnitude(vector_subtract(v, w))
+
+#          user 0  1  2  3  4  5  6  7  8  9
+#
+friendships = [[0, 1, 1, 0, 0, 0, 0, 0, 0, 0], # user 0
+               [1, 0, 1, 1, 0, 0, 0, 0, 0, 0], # user 1
+               [1, 1, 0, 1, 0, 0, 0, 0, 0, 0], # user 2
+               [0, 1, 1, 0, 1, 0, 0, 0, 0, 0], # user 3
+               [0, 0, 0, 1, 0, 1, 0, 0, 0, 0], # user 4
+               [0, 0, 0, 0, 1, 0, 1, 1, 0, 0], # user 5
+               [0, 0, 0, 0, 0, 1, 0, 0, 1, 0], # user 6
+               [0, 0, 0, 0, 0, 1, 0, 0, 1, 0], # user 7
+               [0, 0, 0, 0, 0, 0, 1, 1, 0, 1], # user 8
+               [0, 0, 0, 0, 0, 0, 0, 0, 1, 0]] # user 9
